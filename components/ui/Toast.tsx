a76d580cc710d1d01cb6f896263863709ec14066
Konrad <galankonrad@gmail.com>
2024-08-03 12:20:35 +0200

    styled toast.tsx

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -16,7 +16,7 @@ export const Toast: React.FC<ToastProps> = ({ message, duration = 1000, onClose 
     }, [duration, onClose]);
 
     return (
-        <div className="fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded-md shadow-lg">
+        <div className="text-sm fixed bottom-4 right-4 bg-zinc-800 text-white px-4 py-2 rounded-md shadow-lg font-montserrat">
             {message}
         </div>
     );
